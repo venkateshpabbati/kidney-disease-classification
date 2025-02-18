98777600a2c598afc76bd84ae0d8bca9245a27f8
VenkateshPabbati <venkateshpabbati@outlook.com>
2025-02-18 09:28:13 +0530

    modified:   config/config.yaml 	modified:   dvc.yaml 	modified:   src/cnnClassifier/config/configuration.py 	modified:   src/cnnClassifier/pipeline/prediction.py

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -84,7 +84,7 @@ class ConfigurationManager:
 
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
-            path_of_model="artifacts/training/model.h5",
+            path_of_model="artifacts/training/model.keras",
             training_data="artifacts/data_ingestion/kidney-ct-scan-image",
             mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
             all_params=self.params,
