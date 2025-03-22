a6576540a492d63d868d8b503edfe906cb70963b
VenkateshPabbati <venkateshpabbati@outlook.com>
2025-03-22 21:44:03 +0530

    Update prediction.py

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -3,20 +3,16 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import os
 
-
-
 class PredictionPipeline:
     def __init__(self,filename):
         self.filename =filename
-
-
     
     def predict(self):
         # load model
         model = load_model(os.path.join("model", "model.keras"))
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size =(224,224))
+        test_image = image.load_img(imagename, target_size=(224,224))
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis = 0)
         result = np.argmax(model.predict(test_image), axis=1)
@@ -28,4 +24,3 @@ class PredictionPipeline:
         else:
             prediction = 'Normal'
             return [{ "image" : prediction}]
-            
